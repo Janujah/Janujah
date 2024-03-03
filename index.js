9cdf944ef793d99fb23a42da4c4f16ea61b9f7a7
Janujah <janujahsivarattinam@gmail.com>
2024-03-03 11:22:25 +0530

    Plantex updated

--- index.js
+++ index.js
@@ -12,3 +12,37 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  var paginationContainer = document.querySelector('.pagination');
+  var pages = document.querySelectorAll('.page');
+  var dotsCount = pages.length;
+
+  function showPage(pageIndex) {
+      pages.forEach(function(page, index) {
+          page.style.display = index === pageIndex ? 'block' : 'none';
+      });
+      updateActiveDot(pageIndex);
+  }
+
+  function updateActiveDot(index) {
+      document.querySelectorAll('.dot').forEach(function(dot, dotIndex) {
+          if(dotIndex === index) {
+              dot.classList.add('active');
+          } else {
+              dot.classList.remove('active');
+          }
+      });
+  }
+
+  for (let i = 0; i < dotsCount; i++) {
+      let dot = document.createElement('span');
+      dot.classList.add('dot');
+      dot.addEventListener('click', function() {
+          showPage(i);
+      });
+      paginationContainer.appendChild(dot);
+  }
+
+  showPage(0);
+});
